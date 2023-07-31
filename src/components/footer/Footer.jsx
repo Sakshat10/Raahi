ef1f8d93db53380b3ee1141bdaa27420920e57e7
Sakshat Kumar <112819804+Sakshat10@users.noreply.github.com>
2023-07-31 13:53:22 +0530

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
-import image from "../images/footer14.jpg";
+// import image from "../images/footer14.jpg";
+import {SlSocialInstagram, SlSocialLinkedin, SlSocialFacebook,SlSocialTwitter} from 'react-icons/sl'
 import "./style.css"
 
 const Footer = () => {
   return (
     <div className='footer' name='Sponsors'>
-    <div className='Footer-image'>
-      <div className='blink'>
-        <marquee behavior="scroll" direction="left" scrollamount='12'>Thanks For Visiting Our Website</marquee>
-      </div>
+      {/* <img src={image} alt="footer-img"  className='footer-image'/> */}
     <hr className='ruler'/>
     <div className='Footer-down'>
       <div className='copyright'>
-        All rights reserved &copy;
+        <div>
+        RAAHI |
       </div>
-      <div className='Footer-logo'>
-        GOTOEGYPT
+      <div>
+         All rights reserved &copy;
+      </div>
       </div>
       <div className='Footer-links'>
-        <p>Privacy</p>
-        <p>Disclaimer</p>
-        <p>Contact</p>
+        <SlSocialInstagram/>
+        <SlSocialLinkedin/>
+        <SlSocialFacebook/>
+        <SlSocialTwitter/>
       </div>
-    </div>
     </div>
     </div>
   )
